3547e8d2aefe086bf2bb01167989d1df51eb6127
joschobart <github@schicker.guru>
2024-02-29 23:12:06 +0100

    testing scheduler

--- fun_with_flags/helperf.py
+++ fun_with_flags/helperf.py
@@ -115,6 +115,7 @@ def get_challengeable_teams_list(
             isWeekendFriendly=weekend_friendly,
         )
 
+        print(challengeable_teams)
         challengeable_teams = api.ht_get_challengeable_teams(challengeable_teams)
 
         for team in challengeable_teams:
